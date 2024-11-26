2e5a1b687135d2a746d4797671d71085ca8ab40b
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-11-26 20:28:35 -0300

    feat: create product route

---
+++ route/product.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const productController = require("../controller/product");
+
+const productRouter = express.Router();
+
+productRouter.get("/", (req, res) => {
+    try {
+        const products = productController.getAllProducts();
+        console.log(products);
+        return res.status(200).json(products);
+    } catch (error) {
+        return res.status(500).json({message: "Erro ao buscar todos os produtos.", details: e.message})
+    }
+});
+
+module.exports = productRouter;
