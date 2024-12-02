b3ce9ec1f1f513401cfd11815060411f9f745b05
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-02 11:16:19 -0300

    feat: add purchase validator

---
+++ src/validators/purchaseValidator.js
@@ -0,0 +1,28 @@
+const z = require("zod");
+
+const PurchaseSchema = z.object({
+    id_product: z
+        .number()
+        .int(),
+    id_client: z
+        .number()
+        .int(),
+    total: z
+        .number()
+        .positive({ message: 'Total must be greater than zero'}),
+});
+
+const validatePurchase = (data) => {
+    try {
+        const parsedPurchase = PurchaseSchema.parsed(data);
+        return { isValid: true, data: parsedPurchase };
+    } catch (error) {
+        
+        const errors = error.errors.map(err => ({
+            field: err.path.join('.'),
+            message: err.message
+        }));
+        return { isValid: false, errors };
+    }
+}
+
