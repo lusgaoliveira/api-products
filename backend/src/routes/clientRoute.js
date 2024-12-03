e7c7b330589df24298c756937ca251ed8aa121f4
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-03 14:41:33 -0300

    feat: add disable route on client route

--- backend/src/routes/clientRoute.js
+++ backend/src/routes/clientRoute.js
@@ -4,5 +4,5 @@ const ClientController = require("../controllers/clientController");
 const ClientRouter = express.Router();
 
 ClientRouter.post("/register", ClientController.create);
-
+ClientRouter.delete("/disable/:id", ClientController.innactive);
 module.exports = ClientRouter;
