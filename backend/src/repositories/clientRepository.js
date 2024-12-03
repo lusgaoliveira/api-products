f80d95dcee1d18f5188dc70a7f0cee0b0ddf6f7a
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-03 15:32:16 -0300

    feat: add update function on repository

--- backend/src/repositories/clientRepository.js
+++ backend/src/repositories/clientRepository.js
@@ -52,6 +52,34 @@ class ClientRepository {
             throw error;
         }
     }
+
+    static async update(id, data) {
+        const db = await connectDB();
+        try {
+            const validatedClient = validateClient(data);
+            if (!validatedClient.isValid) {
+                console.error('Erro na validação:', validatedClient.errors); 
+                throw new Error('Client data is invalid');
+            }
+            
+            const query = `
+                UPDATE clients 
+                SET name = $1, email = $2
+                WHERE id = $3
+                RETURNING *;
+            `;
+            const result = await db.query(query, [
+                validatedClient.data.name, 
+                validatedClient.data.email,
+                id
+            ]);
+            if(result.rowCount === 0) throw new Error('Id client not found');
+            return result.rows[0];
+        } catch (error) {
+            console.error('Error in the update client function:', error.message);
+            throw error;
+        }
+    }
 }
 
 module.exports = ClientRepository;
