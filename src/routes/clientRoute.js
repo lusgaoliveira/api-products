9102ff64522e9b618a8b651743679eb10393935f
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-03 01:09:21 -0300

    chore: put project on backend folder deleted previues files

--- src/routes/clientRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const ClientController = require("../controllers/clientController");
-const ClientRouter = express.Router();
-
-ClientRouter.post("/register", ClientController.create);
-
-module.exports = ClientRouter;
