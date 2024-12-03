ccc022f9c4a6734b8d1743474c2a536cf154860d
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-03 16:28:51 -0300

    feat: add find all and by id routes

--- backend/src/routes/clientRoute.js
+++ backend/src/routes/clientRoute.js
@@ -6,5 +6,7 @@ const ClientRouter = express.Router();
 ClientRouter.post("/register", ClientController.register);
 ClientRouter.delete("/disable/:id", ClientController.disable);
 ClientRouter.put("/update/:id", ClientController.updateClient);
+ClientRouter.get("/:id", ClientController.findById);
+ClientRouter.get("/", ClientController.findAllClients);
 
 module.exports = ClientRouter;
