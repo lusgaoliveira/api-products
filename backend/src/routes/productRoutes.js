e3e187dd6abad6a8d15e48e69821db96ef09ba5e
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-04 14:51:40 -0300

    feat: add disabe route on product routes

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -4,5 +4,7 @@ const ProductController = require('../controllers/productController');
 const ProductRouter = express.Router();
 
 ProductRouter.post("/register", ProductController.register);
+ProductRouter.delete("/disable/:id", ProductController.disable);
+
 
 module.exports = ProductRouter;
