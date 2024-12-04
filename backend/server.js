f89696cf34e1e840c393e415e68bc3f31fde370a
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-04 13:49:20 -0300

    feat: add routes of produts on server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
 const clientsRoutes = require("./src/routes/clientRoute.js");
+const productsRoutes = require('./src/routes/productRoutes.js');
 const PORT = 4000;
 
 const app = express();
 app.use(express.json());
 
 app.use("/clients", clientsRoutes);
+app.use('/products', productsRoutes);
 
 app.use("/", (req, res) => {
     return res.send("Api de Gerenciamento")
