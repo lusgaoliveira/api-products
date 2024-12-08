858205ef9380ce1eb96ded6c5c78c38a37b14f72
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-08 01:19:20 -0300

    feat: add find all purchases on purchase routes

--- backend/src/routes/purchaseRoutes.js
+++ backend/src/routes/purchaseRoutes.js
@@ -4,5 +4,5 @@ const PurchaseController = require('../controllers/purchaseController');
 const PurchaseRouter = express.Router();
 
 PurchaseRouter.post("/register", PurchaseController.register);
-
+PurchaseRouter.get("/", PurchaseController.findAllPurchases)
 module.exports = PurchaseRouter;
