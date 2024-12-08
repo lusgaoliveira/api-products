08f482ac6cbb0bc80350ccb27285fd3d6cef659b
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-07 23:46:51 -0300

    feat: create purchase routes and add register route

---
+++ backend/src/routes/purchaseRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const PurchaseController = require('../controllers/purchaseController');
+const PurchaseRouter = express.Router();
+
+PurchaseRouter.post("/register", PurchaseController.register);
+
+module.exports = PurchaseRouter;
