558d3f2567154c961be8880b0352a97e03b08945
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-11 08:37:34 -0300

    fix: add id_pruchase on model

--- backend/src/models/purchase.js
+++ backend/src/models/purchase.js
@@ -1,5 +1,6 @@
 class Purchase {
-    constructor(id_product, id_client, total, status = 'finished') {
+    constructor(id_purchase, id_product, id_client, total, status = 'finished') {
+        this.id_purchase = id_purchase;
         this.id_product = id_product;
         this.id_client = id_client;
         this.total = total;
